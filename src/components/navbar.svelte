f62b52cc2769d11dd9f3e007b1d97c15fb330c6f
fauzan-rizky <fauzan.rizky.naufal@gmail.com>
2026-08-17 14:55:48 +0700

    Changed frontend to svelte, init

---
+++ src/components/navbar.svelte
@@ -0,0 +1,49 @@
+<script>
+    import placeholder_logo from "../images/furinanerd-placeholder.jpg"
+</script>
+<div class="navbar">
+    <div class="navbar-sections">
+        <img src={placeholder_logo} alt="furinanerd">
+    </div>
+    <div class="navbar-sections navbar-links">
+        <a class="navbar-links-entry" href="./"><h3>Home</h3></a>
+        <a class="navbar-links-entry" href="./"><h3>About</h3></a>
+        <a class="navbar-links-entry" href="./"><h3>Search</h3></a>
+    </div>
+    <div class="navbar-sections"><h3 class="navbar-links-entry">Gabung</h3></div>
+</div>
+
+<style>
+    .navbar {
+        width: 100%;
+        height: 75px;
+        display: flex;
+
+        justify-content: center;
+        position: fixed;
+
+        background-color: rgb(0, 222, 0);
+    }
+
+    .navbar-sections {
+        width: 33%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: inherit;
+    }
+
+    .navbar-links {
+        gap: 25px;
+    }
+
+    .navbar-links-entry {
+        padding: 5px 7.5px 5px 7.5px;
+        text-decoration: none;
+    }
+
+    img {
+        width: 50px;
+        height: 50px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+import "../styles/global.css"
+import placeholder_logo from "../images/furinanerd-placeholder.jpg"
+import Navbar from "../components/navbar.svelte";
+</script>
+<Navbar></Navbar>
+<div class="screen-div">
+<img id="logo-kerjain-home" src={placeholder_logo} alt="furinanerd">
+<p>(placeholder logo /\)</p>
+<h1>
+    KERJAIN
+</h1>
+<h2>
+    Sistem terintegrasi lorem ipsum
+</h2>
+<h2>
+    Dolor sit amet
+</h2>
+</div>
+
+<style>
+    #logo-kerjain-home {
+        width: 150px;
+        margin-bottom: 25px;
+    }
+</style>
