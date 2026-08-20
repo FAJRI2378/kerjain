df2640c6ddb51d40445747adb34fc4481c535fbc
FAJRI2378 <armanfajri008@gmail.com>
2026-08-20 20:54:12 +0700

    fix: enforce vercel adapter setup

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'; // <-- Import paket Tailwind v4
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
-		tailwindcss(), // <-- Dipanggil dengan benar di sini
-		sveltekit({
-			compilerOptions: {
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			}
-		})
+		tailwindcss(),
+		sveltekit()
 	]
 });
